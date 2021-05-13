e437b176b873cd258285b98f15dc594a2749ab53
Andrei Brehuescu <Andrei.Brehuescu@msg.group>
2021-05-13 15:08:50 +0300

    Update Api.tsx

--- event-management-frontend/src/api/Api.tsx
+++ event-management-frontend/src/api/Api.tsx
@@ -1,2 +1,2 @@
-export const serverURL = 'http://ec2-34-241-247-91.eu-west-1.compute.amazonaws.com:8080';
+export const serverURL = 'http://ec2-54-194-204-238.eu-west-1.compute.amazonaws.com:8080';
 export const s3URL = 'https://event-management-pictures.s3-eu-west-1.amazonaws.com';
