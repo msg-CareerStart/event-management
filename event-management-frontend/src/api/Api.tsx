08a56785c5fd4710955ed0ee8c891fd5accc85be
Szab <sirmrvsbi@gmail.com>
2021-05-20 15:46:07 +0300

    Change backend URL in the frontend configuration

--- event-management-frontend/src/api/Api.tsx
+++ event-management-frontend/src/api/Api.tsx
@@ -1,4 +1,4 @@
-export const serverURL = 'http://ec2-18-200-143-19.eu-west-1.compute.amazonaws.com:8080';
+export const serverURL = 'ec2-54-246-65-51.eu-west-1.compute.amazonaws.com:8080';
 export const s3URL = 'https://event-management-pictures.s3-eu-west-1.amazonaws.com';
 
 // new aws links ->
