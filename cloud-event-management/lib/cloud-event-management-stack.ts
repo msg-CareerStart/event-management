012bee2ca534ddca22c87e7b1f73a6a85c8dc0b2
Andrei Stefanie <Andrei.Stefanie@msg.group>
2021-05-24 19:52:49 +0300

    Use proper EC2 Image for ECS and cleanup

--- cloud-event-management/lib/cloud-event-management-stack.ts
+++ cloud-event-management/lib/cloud-event-management-stack.ts
@@ -3,45 +3,29 @@ import * as ec2 from '@aws-cdk/aws-ec2';
 import * as ecs from '@aws-cdk/aws-ecs';
 import * as rds from '@aws-cdk/aws-rds';
 import { AutoScalingGroup } from '@aws-cdk/aws-autoscaling';
-import {
-  ApplicationLoadBalancer,
-  ApplicationProtocol,
-} from '@aws-cdk/aws-elasticloadbalancingv2';
-import * as logs from '@aws-cdk/aws-logs';
+import { ApplicationLoadBalancer } from '@aws-cdk/aws-elasticloadbalancingv2';
 import * as acm from '@aws-cdk/aws-certificatemanager';
 
 export class CloudEventManagementStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    cdk.Tags.of(this).add('cloud', 'event-management');
+    cdk.Tags.of(this).add('project', 'event-management');
 
-    //vpc
-    //our network env
-    const vpc = new ec2.Vpc(this, 'VPC');
-
-    //security groups, telling comps what to do
-
-    // A load balancer distributes incoming application traffic across multiple EC2 instances in multiple Availability Zones.
-    //This increases the fault tolerance of your applications.
+    const vpc = new ec2.Vpc(this, 'VPC', { natGateways: 0 });
 
     //load balancer security group
     const lbSG = new ec2.SecurityGroup(this, 'LB-SG', {
       vpc,
-      allowAllOutbound: true,
       description: 'Security group for the load balancer',
     });
-
-    //ingress and egress rules let us specify where we get or deliver things
 
     lbSG.addIngressRule(
       ec2.Peer.anyIpv4(),
       ec2.Port.tcp(443),
       'Allow HTTP traffic for the load balancer'
     );
-    //lbSG.addEgressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'Allow HTTP traffic for the load balancer');
 
-    // app security group
     const appSG = new ec2.SecurityGroup(this, 'App-SG', {
       vpc,
       description: 'Security group for the application',
@@ -49,8 +33,8 @@ export class CloudEventManagementStack extends cdk.Stack {
 
     appSG.addIngressRule(
       lbSG,
-      ec2.Port.tcp(8080),
-      'Allow HTTP traffic from the load balancer'
+      ec2.Port.allTcp(),
+      'Allow traffic from the load balancer'
     );
 
     //db sg
@@ -65,20 +49,15 @@ export class CloudEventManagementStack extends cdk.Stack {
       'Allow My sql traffic from the application'
     );
 
-    // app tier - auto scaling group
-    // auto scaling for our app
-
     const asg = new AutoScalingGroup(this, 'ASG', {
+      vpc,
       instanceType: ec2.InstanceType.of(
         ec2.InstanceClass.T3,
         ec2.InstanceSize.MICRO
       ),
-      machineImage: ec2.MachineImage.genericLinux({
-        'eu-west-1': 'ami-063d4ab14480ac177',
-      }),
-      vpc,
+      machineImage: ecs.EcsOptimizedImage.amazonLinux2(),
       minCapacity: 1,
-      maxCapacity: 2,
+      maxCapacity: 1,
       securityGroup: appSG,
       vpcSubnets: { subnets: vpc.publicSubnets },
     });
@@ -98,37 +77,25 @@ export class CloudEventManagementStack extends cdk.Stack {
 
     // Create Task Definition
     const taskDefinition = new ecs.Ec2TaskDefinition(this, 'TaskDef');
-    const container = taskDefinition.addContainer('web', {
+    taskDefinition.addContainer('web', {
       image: ecs.ContainerImage.fromRegistry(
-        'erwinhilbert/event-management:testV2'
+        'erwinhilbert/event-management:https'
       ),
-      logging: ecs.LogDrivers.awsLogs({
-        streamPrefix: 'backend_2231212',
-        logGroup: new logs.LogGroup(this, 'logs', {
-          logGroupName: 'backend_2231212',
-        }),
-      }),
       memoryLimitMiB: 512,
       environment: {
         //https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/java-rds.html#java-rds-javase
         SPRING_PROFILES_ACTIVE: 'test',
         //rds to do
       },
+      portMappings: [{ containerPort: 8080 }],
     });
 
-    container.addPortMappings({
-      containerPort: 8080,
-      protocol: ecs.Protocol.TCP,
-    });
-
-    // Create Service
     const service = new ecs.Ec2Service(this, 'Service', {
       cluster,
       taskDefinition,
     });
 
-    // Create ALB
-    const lb = new ApplicationLoadBalancer(this, 'LB', {
+    const lb = new ApplicationLoadBalancer(this, 'ALB', {
       vpc,
       internetFacing: true,
       securityGroup: lbSG,
@@ -145,31 +112,18 @@ export class CloudEventManagementStack extends cdk.Stack {
       port: 443,
       open: true,
       certificates: [cert],
-      protocol: ApplicationProtocol.HTTPS,
     });
 
     // Attach ALB to ECS Service
     listener.addTargets('ECS', {
-      port: 8080,
-      protocol: ApplicationProtocol.HTTP,
-      targets: [
-        service.loadBalancerTarget({
-          containerName: 'web',
-          containerPort: 8080,
-        }),
-      ],
-      /*
+      port: 80,
+      targets: [service],
       healthCheck: {
         enabled: true,
         healthyHttpCodes: '403',
         path: '/',
       },
-      */
-      // if i remove the health check i get draining, if i let it with 403 i get unhealthy
     });
-
-    //security policy
-    // ssl certificate for http and https
 
     new cdk.CfnOutput(this, 'LoadBalancerDNS', {
       value: lb.loadBalancerDnsName,
