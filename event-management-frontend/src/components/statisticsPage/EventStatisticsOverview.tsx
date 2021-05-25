9d0947ed1b317204adba7689028f948089836cb0
Andrei Brehuescu <Andrei.Brehuescu@msg.group>
2021-05-25 12:36:45 +0300

    Create EventStatisticsOverview.tsx
    Overview component for the event statistic

---
+++ event-management-frontend/src/components/statisticsPage/EventStatisticsOverview.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+interface EventStatisticsPropd {
+  events: [];
+}
+
+function EventStatisticsOverview(props: EventStatisticsPropd) {
+  const { t } = useTranslation();
+}
