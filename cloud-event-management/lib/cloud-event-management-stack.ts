2474f08f5dc08b6207264f746cf054d6b76b1a1d
Andrei Stefanie <andrei.stefanie@gmail.com>
2021-05-31 18:02:00 +0300

    Refactored the entire stack + parametrization

--- cloud-event-management/lib/cloud-event-management-stack.ts
+++ cloud-event-management/lib/cloud-event-management-stack.ts
@@ -10,16 +10,72 @@ import {
 import * as acm from '@aws-cdk/aws-certificatemanager';
 import { Repository } from '@aws-cdk/aws-ecr';
 import { Duration } from '@aws-cdk/core';
+import { Secret } from '@aws-cdk/aws-secretsmanager';
 
 export class CloudEventManagementStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const certificateArn = new cdk.CfnParameter(this, 'certificateARN', {
+      description:
+        'The ARN of the certificate used for load balancer HTTPS listener',
+    });
+
+    const imageTag = new cdk.CfnParameter(this, 'imageTag', {
+      description:
+        'The tag of the docker image from ECR',
+    });
+
     cdk.Tags.of(this).add('project', 'event-management');
 
     const vpc = new ec2.Vpc(this, 'VPC', { natGateways: 0 });
 
-    //load balancer security group
+    const { appSG, dbSG, lbSG } = this.setupSecurityGroups(vpc);
+
+    const asg = this.setupASG(vpc, appSG);
+
+    const db = this.setupRDS(dbSG);
+
+    const service = this.setupECS(vpc, asg, db, imageTag.valueAsString);
+
+    const lb = this.setupLoadBalancer(
+      vpc,
+      lbSG,
+      service,
+      certificateArn.valueAsString
+    );
+
+    new cdk.CfnOutput(this, 'LoadBalancerDNS', {
+      value: lb.loadBalancerDnsName,
+    });
+  }
+
+  private setupASG(vpc: ec2.Vpc, appSG: ec2.SecurityGroup) {
+    return new AutoScalingGroup(this, 'ASG', {
+      vpc,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T3,
+        ec2.InstanceSize.MICRO
+      ),
+      machineImage: ecs.EcsOptimizedImage.amazonLinux2(),
+      minCapacity: 1,
+      maxCapacity: 1,
+      securityGroup: appSG,
+      vpcSubnets: { subnets: vpc.publicSubnets },
+    });
+  }
+
+  private setupRDS(dbSG: ec2.SecurityGroup) {
+    return rds.DatabaseInstance.fromDatabaseInstanceAttributes(this, 'db', {
+      securityGroups: [dbSG],
+      port: 3306,
+      instanceEndpointAddress:
+        'event-management-db.c1i49kbi7ss8.eu-west-1.rds.amazonaws.com',
+      instanceIdentifier: 'event-management-db',
+    });
+  }
+
+  private setupSecurityGroups(vpc: ec2.Vpc) {
     const lbSG = new ec2.SecurityGroup(this, 'LB-SG', {
       vpc,
       description: 'Security group for the load balancer',
@@ -45,39 +101,27 @@ export class CloudEventManagementStack extends cdk.Stack {
     //db sg
     const dbSG = new ec2.SecurityGroup(this, 'DB-SG', {
       vpc,
-      description: 'SG for the application',
+      description: 'SG for the database',
     });
 
     dbSG.addIngressRule(
       appSG,
       ec2.Port.tcp(3306),
-      'Allow My sql traffic from the application'
+      'Allow MySQL traffic from the application'
     );
+    return { appSG, dbSG, lbSG };
+  }
 
-    const asg = new AutoScalingGroup(this, 'ASG', {
+  private setupECS(
+    vpc: ec2.Vpc,
+    asg: AutoScalingGroup,
+    db: rds.IDatabaseInstance,
+    tag: string
+  ) {
+    const cluster = new ecs.Cluster(this, 'EcsCluster', {
       vpc,
-      instanceType: ec2.InstanceType.of(
-        ec2.InstanceClass.T3,
-        ec2.InstanceSize.MICRO
-      ),
-      machineImage: ecs.EcsOptimizedImage.amazonLinux2(),
-      minCapacity: 1,
-      maxCapacity: 1,
-      securityGroup: appSG,
-      vpcSubnets: { subnets: vpc.publicSubnets },
+      clusterName: 'event-management-cluster',
     });
-
-    //dbSecret.grantRead(asg.role);
-
-    rds.DatabaseInstance.fromDatabaseInstanceAttributes(this, 'db', {
-      securityGroups: [dbSG],
-      port: 3306,
-      instanceEndpointAddress:
-        'event-management-db.c1i49kbi7ss8.eu-west-1.rds.amazonaws.com',
-      instanceIdentifier: 'event-management-db',
-    });
-
-    const cluster = new ecs.Cluster(this, 'EcsCluster', { vpc });
     const capacityProvider = new ecs.AsgCapacityProvider(
       this,
       'AsgCapacityProvider',
@@ -90,7 +134,13 @@ export class CloudEventManagementStack extends cdk.Stack {
     // Create Task Definition
     const image = ecs.ContainerImage.fromEcrRepository(
       Repository.fromRepositoryName(this, 'ECR-Repo', 'event-managment'),
-      'a5cbdf63a8b3b4d7cdcc33b7eca3864f6ee71014'
+      tag
+    );
+
+    const dbSecret = Secret.fromSecretNameV2(
+      this,
+      'DB-Secret',
+      'prodDB'
     );
 
     const taskDefinition = new ecs.Ec2TaskDefinition(this, 'TaskDef');
@@ -98,18 +148,31 @@ export class CloudEventManagementStack extends cdk.Stack {
       image,
       memoryLimitMiB: 512,
       environment: {
-        //https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/java-rds.html#java-rds-javase
-        SPRING_PROFILES_ACTIVE: 'test',
-        //rds to do
+        SPRING_PROFILES_ACTIVE: 'prod',
+        DB_URL: db.instanceEndpoint.hostname
+      },
+      secrets: {
+        DB_USERNAME: ecs.Secret.fromSecretsManager(dbSecret, 'username'),
+        DB_PASSWORD: ecs.Secret.fromSecretsManager(dbSecret, 'password'),
       },
       portMappings: [{ containerPort: 8080, hostPort: 8080 }],
+      logging: ecs.LogDrivers.awsLogs({ streamPrefix: 'event-management-ecs' })
     });
 
     const service = new ecs.Ec2Service(this, 'Service', {
       cluster,
       taskDefinition,
+      serviceName: 'event-management-cluster',
     });
+    return service;
+  }
 
+  private setupLoadBalancer(
+    vpc: ec2.Vpc,
+    lbSG: ec2.SecurityGroup,
+    service: ecs.Ec2Service,
+    certificateArn: string
+  ) {
     const lb = new ApplicationLoadBalancer(this, 'ALB', {
       vpc,
       internetFacing: true,
@@ -120,7 +183,7 @@ export class CloudEventManagementStack extends cdk.Stack {
     const cert = acm.Certificate.fromCertificateArn(
       this,
       'Certificate',
-      'arn:aws:acm:eu-west-1:026709880083:certificate/8095d922-a477-4c1d-8215-b1c0e3378588'
+      certificateArn
     );
 
     const listener = lb.addListener('PublicHTTPSListener', {
@@ -141,9 +204,6 @@ export class CloudEventManagementStack extends cdk.Stack {
         interval: Duration.seconds(300),
       },
     });
-
-    new cdk.CfnOutput(this, 'LoadBalancerDNS', {
-      value: lb.loadBalancerDnsName,
-    });
+    return lb;
   }
 }
